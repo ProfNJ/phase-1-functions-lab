7f774f20fb70fc2c899e3b4e29cac6cdcc1ad983
Rory <rorygran5@gmail.com>
2022-04-25 21:42:24 -0400

    commit tree aeec3cfe9f76d16c846b3099ba487bf3d0a4ebf3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+
+const a = 42;
+const conversion = 264;
+function distanceFromHqInBlocks(b){
+ return Math.abs(b - a);
+}
+
+function distanceFromHqInFeet(b){
+    return distanceFromHqInBlocks(b) * conversion;
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(end - start) * conversion;
+
+}
+
+function calculatesFarePrice(start, end){
+    let fare = distanceTravelledInFeet(start, end);
+    if (fare < 400){
+        return 0;
+    }else if(fare > 2500){
+        return "cannot travel that far"
+    }else{
+        return fare > 2000? 25:(fare - 400) * .02;
+    }
+}
 // Code your solution in this file!
